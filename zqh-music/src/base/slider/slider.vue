7f8cb83e2b0ba233607b4cfe63ed9b59bf36a30f
zhuqihui <zhuqihui>
2018-05-04 17:44:06 +0800

    用新版本better-scroll实现轮播图

--- zqh-music/src/base/slider/slider.vue
+++ zqh-music/src/base/slider/slider.vue
@@ -1,21 +1,22 @@
 <template>
-  <div class="slider" ref="slider">
-    <div class="slider-group" ref="sliderGroup">
+  <div class="slide" ref="slide">
+    <div class="slide-group" ref="slideGroup">
       <slot>
       </slot>
     </div>
-    <div class="dots">
+    <div v-if="showDot" class="dots">
       <span class="dot" :class="{active: currentPageIndex === index }" v-for="(item, index) in dots"></span>
     </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  import {addClass} from 'common/js/dom'
+  import {addClass} from '../../common/js/dom'
   import BScroll from 'better-scroll'
 
+  const COMPONENT_NAME = 'slide'
   export default {
-    name: 'slider',
+    name: COMPONENT_NAME,
     props: {
       loop: {
         type: Boolean,
@@ -28,6 +29,22 @@
       interval: {
         type: Number,
         default: 4000
+      },
+      showDot: {
+        type: Boolean,
+        default: true
+      },
+      click: {
+        type: Boolean,
+        default: true
+      },
+      threshold: {
+        type: Number,
+        default: 0.3
+      },
+      speed: {
+        type: Number,
+        default: 400
       }
     },
     data() {
@@ -37,108 +54,160 @@
       }
     },
     mounted() {
-      setTimeout(() => {
-        this._setSliderWidth()
-        this._initDots()
-        this._initSlider()
-
-        if (this.autoPlay) {
-          this._play()
-        }
-      }, 20)
-
+      this.update()
       window.addEventListener('resize', () => {
-        if (!this.slider) {
+        if (!this.slide || !this.slide.enabled) {
           return
         }
-        this._setSliderWidth(true)
-        this.slider.refresh()
+        clearTimeout(this.resizeTimer)
+        this.resizeTimer = setTimeout(() => {
+          if (this.slide.isInTransition) {
+            this._onScrollEnd()
+          } else {
+            if (this.autoPlay) {
+              this._play()
+            }
+          }
+          this.refresh()
+        }, 60)
       })
     },
     activated() {
+      if (!this.slide) {
+        return
+      }
+      this.slide.enable()
+      let pageIndex = this.slide.getCurrentPage().pageX
+      this.slide.goToPage(pageIndex, 0, 0)
+      this.currentPageIndex = pageIndex
       if (this.autoPlay) {
         this._play()
       }
     },
     deactivated() {
+      this.slide.disable()
       clearTimeout(this.timer)
     },
     beforeDestroy() {
+      this.slide.disable()
       clearTimeout(this.timer)
     },
     methods: {
-      _setSliderWidth(isResize) {
-        this.children = this.$refs.sliderGroup.children
-
+      update() {
+        if (this.slide) {
+          this.slide.destroy()
+        }
+        this.$nextTick(() => {
+          this.init()
+        })
+      },
+      refresh() {
+        this._setSlideWidth(true)
+        this.slide.refresh()
+      },
+      prev() {
+        this.slide.prev()
+      },
+      next() {
+        this.slide.next()
+      },
+      init() {
+        clearTimeout(this.timer)
+        this.currentPageIndex = 0
+        this._setSlideWidth()
+        if (this.showDot) {
+          this._initDots()
+        }
+        this._initSlide()
+        if (this.autoPlay) {
+          this._play()
+        }
+      },
+      _setSlideWidth(isResize) {
+        this.children = this.$refs.slideGroup.children
         let width = 0
-        let sliderWidth = this.$refs.slider.clientWidth
+        let slideWidth = this.$refs.slide.clientWidth
         for (let i = 0; i < this.children.length; i++) {
           let child = this.children[i]
-          addClass(child, 'slider-item')
-
-          child.style.width = sliderWidth + 'px'
-          width += sliderWidth
+          addClass(child, 'slide-item')
+          child.style.width = slideWidth + 'px'
+          width += slideWidth
         }
         if (this.loop && !isResize) {
-          width += 2 * sliderWidth
+          width += 2 * slideWidth
         }
-        this.$refs.sliderGroup.style.width = width + 'px'
+        this.$refs.slideGroup.style.width = width + 'px'
       },
-      _initSlider() {
-        this.slider = new BScroll(this.$refs.slider, {
+      _initSlide() {
+        console.log(this.threshold)
+        this.slide = new BScroll(this.$refs.slide, {
           scrollX: true,
           scrollY: false,
           momentum: false,
-          snap: true,
-          snapLoop: this.loop,
-          snapThreshold: 0.3,
-          snapSpeed: 400
+          snap: {
+            loop: this.loop,
+            threshold: this.threshold,
+            speed: this.speed
+          },
+          bounce: false,
+          stopPropagation: true,
+          click: this.click
         })
-
-        this.slider.on('scrollEnd', () => {
-          let pageIndex = this.slider.getCurrentPage().pageX
-          if (this.loop) {
-            pageIndex -= 1
-          }
-          this.currentPageIndex = pageIndex
-
+        this.slide.on('scrollEnd', this._onScrollEnd)
+        this.slide.on('touchEnd', () => {
           if (this.autoPlay) {
             this._play()
           }
         })
-
-        this.slider.on('beforeScrollStart', () => {
+        this.slide.on('beforeScrollStart', () => {
           if (this.autoPlay) {
             clearTimeout(this.timer)
           }
         })
       },
+      _onScrollEnd() {
+        let pageIndex = this.slide.getCurrentPage().pageX
+        this.currentPageIndex = pageIndex
+        if (this.autoPlay) {
+          this._play()
+        }
+      },
       _initDots() {
         this.dots = new Array(this.children.length)
       },
       _play() {
-        let pageIndex = this.currentPageIndex + 1
-        if (this.loop) {
-          pageIndex += 1
-        }
+        clearTimeout(this.timer)
         this.timer = setTimeout(() => {
-          this.slider.goToPage(pageIndex, 0, 400)
+          this.slide.next()
         }, this.interval)
+      }
+    },
+    watch: {
+      loop() {
+        this.update()
+      },
+      autoPlay() {
+        this.update()
+      },
+      speed() {
+        this.update()
+      },
+      threshold() {
+        this.update()
       }
     }
   }
 </script>
 
-<style scoped lang="stylus" rel="stylesheet/stylus">
-  @import "~common/stylus/variable"
-
-  .slider
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/variable"
+  .slide
     min-height: 1px
-    .slider-group
+    .slide-group
       position: relative
       overflow: hidden
       white-space: nowrap
-      .slider-item
+      .slide-item
         float: left
         box-sizing: border-box
         overflow: hidden
@@ -156,6 +225,7 @@
       right: 0
       left: 0
       bottom: 12px
+      transform: translateZ(1px)
       text-align: center
       font-size: 0
       .dot
