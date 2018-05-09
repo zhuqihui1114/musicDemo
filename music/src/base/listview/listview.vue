8db733aa5cbd3ec75f54d3be53af9acb1e567b4d
zhuqihui <zhuqihui>
2018-05-09 17:14:07 +0800

    歌手列表页完成

--- music/src/base/listview/listview.vue
+++ music/src/base/listview/listview.vue
@@ -1,5 +1,8 @@
 <template>
-  <scroll :data="data"
+  <scroll @scroll="scroll"
+          :listen-scroll="listenScroll"
+          :probe-type="probeType"
+          :data="data"
           class="listview"
           ref="listview">
     <ul>
@@ -21,13 +24,21 @@
         </li>
       </ul>
     </div>
+    <div class="list-fixed" ref="fixed" v-show="fixedTitle">
+      <div class="fixed-title">{{fixedTitle}} </div>
+    </div>
+    <div v-show="!data.length" class="loading-container">
+      <loading></loading>
+    </div>
   </scroll>
 </template>
 
 <script type="text/ecmascript-6">
   import Scroll from '../../base/scroll/scroll'
+  import Loading from '../../base/loading/loading'
   import {getData} from '../../common/js/dom'
 
+  const TITLE_HEIGHT = 30
   const ANCHOR_HEIGHT = 18
 
   export default {
@@ -55,6 +66,12 @@
         return this.data.map((group) => {
           return group.title.substr(0, 1)
         })
+      },
+      fixedTitle() {
+        if (this.scrollY > 0) {
+          return ''
+        }
+        return this.data[this.currentIndex] ? this.data[this.currentIndex].title : ''
       }
     },
     methods: {
@@ -76,6 +93,20 @@
 
         this._scrollTo(anchorIndex)
       },
+      scroll(pos) {
+        this.scrollY = pos.y
+      },
+      _calculateHeight() {
+        this.listHeight = []
+        const list = this.$refs.listGroup
+        let height = 0
+        this.listHeight.push(height)
+        for (let i = 0; i < list.length; i++) {
+          let item = list[i]
+          height += item.clientHeight
+          this.listHeight.push(height)
+        }
+      },
       _scrollTo(index) {
         if (!index && index !== 0) {
           return
@@ -85,13 +116,51 @@
         } else if (index > this.shortcutList.length - 1) {
           index = this.shortcutList.length - 1
         }
+        // 右侧字母条滚动时，焦点跟随
         this.currentIndex = parseInt(index) | 0
 
         this.$refs.listview.scrollToElement(this.$refs.listGroup[index], 0)
       }
     },
+    watch: {
+      data() {
+        setTimeout(() => {
+          this._calculateHeight()
+        }, 20)
+      },
+      scrollY(newY) {
+        const listHeight = this.listHeight
+        // 歌手列表滚动时，右侧焦点跟随
+        // 当滚动到顶部，newY>0
+        if (newY > 0) {
+          this.currentIndex = 0
+          return
+        }
+        // 在中间部分滚动
+        for (let i = 0; i < listHeight.length - 1; i++) {
+          let height1 = listHeight[i]
+          let height2 = listHeight[i + 1]
+          if (-newY >= height1 && -newY < height2) {
+            this.currentIndex = i
+            this.diff = height2 + newY
+            return
+          }
+        }
+        // 当滚动到底部，且-newY大于最后一个元素的上限
+        this.currentIndex = listHeight.length - 2
+      },
+      diff(newVal) {
+        let fixedTop = (newVal > 0 && newVal < TITLE_HEIGHT) ? newVal - TITLE_HEIGHT : 0
+        if (this.fixedTop === fixedTop) {
+          return
+        }
+        this.fixedTop = fixedTop
+        this.$refs.fixed.style.transform = `translate3d(0,${fixedTop}px,0)`
+      }
+    },
     components: {
-      Scroll
+      Scroll,
+      Loading
     }
   }
 
